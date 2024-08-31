dc7caee09627080d7794fb4433d224bb336e4387
Dilmurd <0nishonov.dilmurod@gmail.com>
2024-08-31 23:21:17 +0500

    lesson005

---
+++ js/main.js
@@ -0,0 +1,175 @@
+// // 1-masala
+// {
+//     let n = +prompt ("a sonini kriting")
+//     let k = +prompt ("k sonini kriting")
+//     for(let i = 0; i < n; i++){
+//         console.log(k);
+//     }
+// }
+// // masala 2
+// {
+//     let a = +prompt ("a sonini kriting")
+//     let b = +prompt ("b sonini kriting")
+//     let c = 0
+//     for (let i = a; i <= b; i++){
+//             console.log(i);
+//             c++
+//     }
+// }
+// // 3-masala
+// {
+//     let a = +prompt("a sonini kriting")
+//     let b = +prompt("b sonini kriting")
+//     let c = 0
+//     for(let i = b - 1; i > a; i--){
+//         console.log(i);
+//         c++        
+//     }
+// }
+// // 4-masala
+// {
+//     let a = +prompt ("berilgan konfetning narxini kriting")
+//     let c = 0
+//     for(let i = 1; i <= 10; i++){
+//         c = i * a
+//         console.log(i);
+//     }
+// }
+// // 5-masala
+// {
+//     let a = +prompt ("berilgan konfetning narxini kriting")
+//     let c = 0;
+//     for (let i = 0.1; i <= 1; i += 0.1){
+//         c = i * a;
+//         console.log(i);
+//     }
+// }
+// // 6-masala
+// {
+//     let a = +prompt ("berilgan konfetning narxini kriting")
+//     let c = 0;
+//     for(let i = 0.2; i <= 1; i += 0.2){
+//         c = i * a;
+//         console.log(i);
+//     }
+// }
+// // 7-masala
+// {
+//     let a = +prompt (" a sonini kriting")
+//     let b = +prompt (" b sonini kriting")
+//     let c = 0;
+//     for(let i = a; i <= b; i++){
+//         c += i
+//         console.log(c);
+        
+//     }
+// }
+// // 8-masala
+// {
+//     let a = +prompt (" a sonini kriting")
+//     let b = +prompt (" b sonini kriting")
+//     let c = 1 
+//     for (let i = a; i <= b; i++){
+//         c *= 1
+//         console.log(c);
+//     }
+// }
+// // 9-masala
+// {
+//     let a = +prompt (" a sonini kriting")
+//     let b = +prompt (" b sonini kriting")
+//     let c = 0;
+//     for(let i = a; i <= b; i++){
+//         let d = i * i;
+//         c += d;
+//         console.log(d);
+//         console.log(c);
+//     }
+// }
+// // 10-masala
+// {
+//     let n = +prompt ("n sonini kriting")
+//     let c = 0;
+//     for (let i = 1; i <= n; i++){
+//         c = 1/i;
+//         console.log(c);
+//     }
+// }
+
+                      // While dagi masalalar //
+
+// 1-masala
+// {
+//     let a = +prompt (" a sonini kriting")
+//     let b = +prompt (" b sonini kriting")
+//     while(a > b ){
+//         a -= b;
+//         console.log(a);
+//     }
+// }
+// // 2-masala
+// {
+//     let a = +prompt("a sonini kriting")
+//     let b = +prompt("b sonini kriting")
+//     let c = 0;
+//     while(a > b){
+//         a -= b;
+//         c++;
+//         console.log(c);
+//     }
+// }
+// // 3-masala
+// {
+//     let n = +prompt ("n sonini kriting")
+//     let k = +prompt ("k sonini kriting")
+//     let c = 0;
+//     let d = n;
+//     while(d >= k){
+//         d -= k;
+//         c++
+//         console.log(c);
+//         console.log(d);
+//     }
+// // 4-masala
+// }
+//     let n = +prompt("n sonini kriting");
+//     let k = true;
+
+//     while (n > 1) {
+//         let c = n;
+//         let d = 3;
+//         while (n > d) {
+//             d += d
+//         }
+
+//         if (n == d) {
+//             n -= d
+//         } else {
+//             k = false
+//             break;
+//         }
+//     }
+//     if (k) {
+//         console.log("3 ni darajasi")
+// } 
+// // 5-masala
+// {
+//     let n = +prompt("n sonini kiriting (n > 0)");
+//     let k = 0;
+
+//     while (n > 1) {
+//         n -= n / 2;
+//         k++;
+//         console.log(k);
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
